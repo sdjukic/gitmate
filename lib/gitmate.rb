75938a531f3ced0b98b94d0556df91a813f47086
Slavisa Djukic <ssdjuka@gmail.com>
2015-04-07 16:59:03 -0400

    list_repo() works.

--- lib/gitmate.rb
+++ lib/gitmate.rb
@@ -2,11 +2,24 @@ class Gitmate
 
   def initialize
     @zen = Zen.new
+    @repo = Repos.new
   end
 
   def zen
     puts @zen.message
   end
+
+  def list_repos
+  	repo_json = @repo.get_repos
+  	puts "\e[34m Here are all your repositories:\e[0m"
+    repo_json.each do |repo|
+      puts "\e[35m #{repo["name"]} \e[0m "
+      repo.each do |k, v|
+        puts "#{k} #{v}"
+      end
+    end
+  end
 end
 
 require 'gitmate/zen'
+require 'gitmate/repos'
--- lib/gitmate/repos.rb
+++ lib/gitmate/repos.rb
@@ -4,23 +4,16 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-user_data = YAML.load_file('.secret_config.yaml')
-puts user_data["user"]
+class Repos
+  
+  @@user_data = YAML.load_file('.secret_config.yaml')
+  @@repos_uri = URI("https://api.github.com/users/#{@@user_data["user"]}/repos")
 
-
-repos_uri = URI("https://api.github.com/users/#{user_data["user"]}/repos")
-
-
-req = Net::HTTP::Get.new(repos_uri)
-req.basic_auth("#{user_data["user"]}", "#{user_data["password"]}")
-response = Net::HTTP.get_response(repos_uri)
-
-resp_json = JSON.parse(response.body)
-
-puts "\e[34m Here are all your repositories:\e[0m"
-resp_json.each do |repo|
-  puts "\e[35m #{repo["name"]} \e[0m "
-  repo.each do |k, v|
-    puts "#{k} #{v}"
+  def get_repos
+    req = Net::HTTP::Get.new(@@repos_uri)
+    req.basic_auth("#{@@user_data["user"]}", "#{@@user_data["password"]}")
+    response = Net::HTTP.get_response(@@repos_uri)
+    resp_json = JSON.parse(response.body)
   end
+
 end
