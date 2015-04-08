babe476a3c37058874c21a5ebbcb206b6896857d
Slavisa Djukic <ssdjuka@gmail.com>
2015-04-08 14:52:46 -0400

    Created repos cache. Added option --details that displays deatils for a given repository.

--- lib/gitmate.rb
+++ lib/gitmate.rb
@@ -9,6 +9,7 @@ class Gitmate < Thor
 
   @@zen = Zen.new
   @@repo = Repos.new
+  @@repo_cache = @@repo.get_repos
 
   desc "zen", "Displays random quote from github."
   method_option :desc => "A quote from github to inspire you."
@@ -16,16 +17,28 @@ class Gitmate < Thor
     puts "\e[30m #{@@zen.message} \e[0m"
   end
 
-  desc "list_repos", "Displays all repositories of the user"
-  method_option :desc => "Prints all your repositories"
-  def list_repos
-  	repo_json = @@repo.get_repos
-  	puts "\e[34m Here are all your repositories:\e[0m"
-    repo_json.each do |repo|
-      puts "\e[35m #{repo["name"]} \e[0m "
-      $repo_data["InterestingKeys"].each do |p|
-        puts "\e[32m #{p} #{repo[p]} \e[0m"
-      
+  desc "repos [repository]", "Different actions on your gitrepositories."
+  method_option :list, :aliases => '-l', desc: 'List all repositories'
+  method_option :details, :aliases => '-d', desc: 'Detailed information on a depo'
+  def repos(the_repo=nil)
+  
+    if options[:list]
+  	  puts "\e[34m Here are all your repositories:\e[0m"
+      @@repo_cache.each do |repo|
+        puts "\e[35m #{repo["name"]} \e[0m "
+      end
+    end
+        
+    if options[:details]
+      unless the_repo
+        puts "\e[31m ERROR: missing repository name.\n
+              gitmate repos <repository_name> --details \e[0m "
+      else
+        repo = @@repo_cache.find{ |r| r["name"] == the_repo}
+        puts "\e[34m #{repo["name"]} \e[0m "
+        $repo_data["InterestingKeys"].each do |p|
+          puts "\e[32m #{p} #{repo[p]} \e[0m"
+        end
       end
     end
   end
