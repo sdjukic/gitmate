049c7c7ab11702326e1e19645f9658b444389da9
Slavisa Djukic <Slavisa.Djukic15@myhunter.cuny.edu>
2015-04-13 10:05:50 -0400

    Some old stuff from laptop.

--- lib/gitmate/repos.rb
+++ lib/gitmate/repos.rb
@@ -18,5 +18,15 @@ class Repos
 
   def self.create_repo(repo_name)
   	# will use curl which works and don't have to store user credentials
+    repo_uri = URI("https://api.github.com/user/repos")
+    req = Net::HTTP::Post.new(repo_uri)
+    form_data = { "name" => "#{repo_name}"}.to_json
+    req.set_form_data({'name' => repo_name})
+    req.basic_auth("#{@@user_data["user"]}", "#{@@user_data["password"]}")
+
+    #res = Net::HTTP.post_form(repo_uri, 'name' => repo_name) # this is good form, now find way to authenticate
+    #req.set_form_data({data: form_data})
+    res = Net::HTTP.start(repo_uri.hostname, repo_uri.port){ |http| http.request(req) }
+    puts res.body
   end
 end
